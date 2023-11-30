37e9d2f95ce54aadd6527e2c5618b9a4a34c3971
구환준/모건 <koojun99@naver.com>
2023-11-30 22:46:26 +0900

    Initial commit

---
+++ SimpleChattingService/src/main/java/com/example/simplechattingservice/SimpleChattingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplechattingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChattingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChattingServiceApplication.class, args);
+    }
+
+}
---
+++ SimpleChattingService/src/test/java/com/example/simplechattingservice/SimpleChattingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplechattingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChattingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
